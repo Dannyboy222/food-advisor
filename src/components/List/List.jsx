97ab9dec675b6e28dc97a83274e6b02e9cfe8b6b
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2022-03-02 19:10:23 -0800

    got current location and real restaurants list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -29,17 +29,17 @@ const List = () => {
 				<InputLabel>Type</InputLabel>
 				<Select value={type} onChange={(e) => setType(e.target.value)}>
 					<MenuItem value="restaurants">Restaurants</MenuItem>
-					<MenuItem value="restaurants">Hotels</MenuItem>
-					<MenuItem value="restaurants">Attractions</MenuItem>
+					<MenuItem value="Hotels">Hotels</MenuItem>
+					<MenuItem value="Attractions">Attractions</MenuItem>
 				</Select>
 			</FormControl>
 			<FormControl className={classes.formControl}>
 				<InputLabel>Rating</InputLabel>
 				<Select value={type} onChange={(e) => setRating(e.target.value)}>
-					<MenuItem value={0}>All</MenuItem>
-					<MenuItem value={3}>Above 3.0</MenuItem>
-					<MenuItem value={4}>Above 4.0</MenuItem>
-					<MenuItem value={4.5}>Above 4.5</MenuItem>
+					<MenuItem value="">All</MenuItem>
+					<MenuItem value="3">Above 3.0</MenuItem>
+					<MenuItem value="4">Above 4.0</MenuItem>
+					<MenuItem value="4.5">Above 4.5</MenuItem>
 				</Select>
 			</FormControl>
 			<Grid container spacing={3} className={classes.list}>
